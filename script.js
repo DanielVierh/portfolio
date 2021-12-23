e297ad868c9246dfa33f14b0d86444c16be7bbe5
DanielVierh <DanielVierh@users.noreply.github.com>
2021-12-23 16:16:11 +0100

    Mobile Navbar

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.mainContainer');
+const mobileNavbar = document.querySelector('aside');
 
+let lastYPos = 10;
+let mobileNavBarIsVisible = false;
 let load = 0;
 
 // Startet den Intervall initially
@@ -17,4 +20,17 @@ function blurring() {
     loadText.innerText = `${load}%`;
     bg.style = `filter: blur(${100 - load}px)`;
     loadText.style.opacity = `${(100 - load) / 100}`;
+}
+
+function showMobileNavbar() {
+    if (mobileNavBarIsVisible === false) {
+        lastYPos = window.pageYOffset;
+        window.scrollTo(0, 0);
+        mobileNavBarIsVisible = true;
+        mobileNavbar.classList.add('active');
+    } else {
+        mobileNavBarIsVisible = false;
+        mobileNavbar.classList.remove('active');
+        window.scrollTo(0, lastYPos);
+    }
 }
