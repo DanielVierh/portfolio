e0f3dc66f7ec3f04772f914db6df0cf3a660ca3e
DanielVierh <DanielVierh@users.noreply.github.com>
2022-01-13 13:20:11 +0100

    Kleine Codeanpassungen

--- script.js
+++ script.js
@@ -3,40 +3,11 @@ const bg = document.querySelector('.mainContainer');
 const mobileNavbar = document.querySelector('aside');
 const toggleSwitch = document.getElementById("checkbox");
 const nameField = document.getElementById("nameField");
-let charIterator = 0;
-let myname = 'Hi, I am <Daniel/>';
+let charIterator = 2;
+let myname =  `Hi, I am <br> < Daniel/>`;
 
-window.onload = () => {
-    setInterval(writeName, 5000);
-}
-
-
-const writeName = () => {
-    if (charIterator < myname.length) {
-        nameField.innerHTML += myname.charAt(charIterator);
-        charIterator++;
-        setTimeout(writeName, 100);
-    }else{
-        setTimeout(() => {
-            nameField.innerHTML = '';
-            charIterator = 0;
-        }, 5000);
-    }
-}
-
-
-// Switch Theme
-toggleSwitch.addEventListener("change",  switchTheme, true);
-
-function switchTheme(e) {
-    if(e.target.checked) {
-
-        document.documentElement.setAttribute('data-theme', 'light');
-    }else{
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-}
-
+//########################################################################
+// Start
 
 let lastYPos = 10;
 let mobileNavBarIsVisible = false;
@@ -57,6 +28,38 @@ function blurring() {
     loadText.style.opacity = `${(100 - load) / 100}`;
 }
 
+// Write Animation wird zum ersten mal aufgerufen
+setTimeout(writeName, 2000);
+//########################################################################
+// Schreib Animation
+function writeName() {
+    if(charIterator > myname.length) {
+        setTimeout(() => {
+            charIterator = 2;
+        }, 5000);
+    }else{
+        nameField.innerHTML = myname.slice(0, charIterator);
+        charIterator++;
+    }
+    setTimeout(writeName, 150);
+}
+
+//########################################################################
+// Switch Theme
+toggleSwitch.addEventListener("change",  switchTheme, true);
+
+function switchTheme(e) {
+    if(e.target.checked) {
+
+        document.documentElement.setAttribute('data-theme', 'light');
+    }else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+}
+
+//########################################################################
+// Mobile Nav
+
 function showMobileNavbar() {
     if (mobileNavBarIsVisible === false) {
         mobileNavbar.hidden = false;
@@ -75,11 +78,10 @@ function hideMobileNavBackground() {
     mobileNavBarIsVisible = false;
     mobileNavbar.classList.remove('active');
     window.scrollTo(0, lastYPos);
-    // setTimeout(() => {
         mobileNavbar.hidden = true;
-    // }, 1000);
 }
 
+//########################################################################
 // Vue
 
 const app = Vue.createApp({
